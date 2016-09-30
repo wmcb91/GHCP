d2cd29c4225c1064fd13e9e0a322e7a55006c8e7
wmcb91 <wmcb91@gmail.com>
2016-09-30 08:48:54 -0400

    Complete basic client development setup
    Complete basic setup steps to prepare for development.
    
    - Run npm install.
    - Create api, events and ui JavaScript files for authorization, profile
    and rounds. Include 'use strict' as first line for all.
    - Create SCSS files including breakpoint, breakpoints, colors, layout,
    theme, typography and require necessary files in index.scss
    - Copy and paste index.html from Tic-tac-toe and gut non-authorization
    code.  Comment out all but <h1> in header and footer.

---
+++ assets/scripts/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// use require without a reference to ensure a file is bundled
+require('./example');
+
+const authEvents = require('./auth/events');
+const profileEvents = require('./profile/events');
+const roundsEvents = require('./rounds/events');
+
+$(() => {
+  authEvents.addHandlers();
+  profileEvents.addHandlers();
+  roundsEvents.addHandlers();
+});
