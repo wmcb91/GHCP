0dde77c76cf3d3573b365b24598868e9d383c6e5
wmcb91 <wmcb91@gmail.com>
2016-10-04 15:21:21 -0400

    Run grunt build
    Run grunt build to create new bundle.js and vendor.bundle.js files.

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(27);
+	module.exports = __webpack_require__(30);
 
 
 /***/ },
@@ -9935,7 +9935,7 @@
 
 /***/ },
 
-/***/ 27:
+/***/ 30:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
