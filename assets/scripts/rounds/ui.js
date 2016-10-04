3666150c61491579149910e266818843c1ee2e51
wmcb91 <wmcb91@gmail.com>
2016-10-04 17:50:14 -0400

    Get rounds to truncate after 15 on sign in
    On profile sign in, the rounds table displays a maximum of 15 rounds.
    Submitting rounds adds round on top, but adds beyond 15 round limit.

--- assets/scripts/rounds/ui.js
+++ assets/scripts/rounds/ui.js
@@ -15,30 +15,65 @@ const hideAddRoundField = function () {
   $('#add-round').fadeOut(250);
 };
 
-const roundsToObject = function () {
-  let roundsObject = app.profile.rounds.reduce(function(o, v, i) {
+const reverseRoundsToObject = function () {
+  let roundsObject = app.profile.rounds.reverse().reduce(function(o, v, i) {
     o[i] = v;
     return o;
   }, {});
   return roundsObject;
 };
 
+// const roundsToObject = function () {
+//   // console.log('reverse is', app.profile.rounds.reverse());
+//   let roundsObject = app.profile.rounds.reduce(function(o, v, i) {
+//     o[i] = v;
+//     return o;
+//   }, {});
+//   return roundsObject;
+// };
+
 const populateRounds = function () {
-    let roundsObject = roundsToObject();
-    for (let i = 0; i < app.profile.rounds.length; i++) {
-      $('.previous-rounds')
-        .prepend("<tr class='profile-rounds'><td>"+roundsObject[i].date_played+
-                "</td><td>"+roundsObject[i].course+
-                "</td><td>"+roundsObject[i].rating+
-                "</td><td>"+roundsObject[i].slope+
-                "</td><td>"+roundsObject[i].par+
-                "</td><td>"+roundsObject[i].score+
-                "</td></tr>");
+  let reverseRoundsObject = reverseRoundsToObject();
+  console.log(' reverseRoundsObject is',reverseRoundsObject[0]);
+  console.log(' reverseRoundsObject is',reverseRoundsObject[1]);
+  console.log(' reverseRoundsObject is',reverseRoundsObject[app.profile.rounds.length-2]);
+  console.log(' reverseRoundsObject is',reverseRoundsObject[app.profile.rounds.length-1]);
+  // let max = app.profile.rounds.length - 1;
+  // let min = app.profile.rounds.length - 15;
+  // for (let i = 0; i < app.profile.rounds.length; i++) {
+  // let max = app.profile.rounds.length;
+  for (let i = 0; i < 15; i++) {
+  // for (let i = 15; i > 0; i--) {
+    $('.width-setter')
+      .before("<tr class='profile-rounds'><td>"+reverseRoundsObject[i].date_played+
+              "</td><td>"+reverseRoundsObject[i].course+
+              "</td><td>"+reverseRoundsObject[i].rating+
+              "</td><td>"+reverseRoundsObject[i].slope+
+              "</td><td>"+reverseRoundsObject[i].par+
+              "</td><td>"+reverseRoundsObject[i].score+
+              "</td></tr>");
     }
+};
+
+const addRound = function (data) {
+  let newRoundObject = data;
+  $('.previous-rounds')
+    .prepend("<tr class='profile-rounds'><td>"+newRoundObject.date_played+
+            "</td><td>"+newRoundObject.course+
+            "</td><td>"+newRoundObject.rating+
+            "</td><td>"+newRoundObject.slope+
+            "</td><td>"+newRoundObject.par+
+            "</td><td>"+newRoundObject.score+
+            "</td></tr>");
 };
 
 const clearRounds = function () {
   $('.profile-rounds').html('');
+};
+
+const addRoundSuccess = function () {
+  clearRounds();
+  populateRounds();
 };
 
 // ON SUBMISSION OF VALID ROUND
@@ -50,8 +85,9 @@ const createRoundSuccess = function (data) {
   //Console
   console.log(app.profile.rounds);
   hideAddRoundField();
-  setTimeout(function(){clearRounds();}, 250);
-  setTimeout(function(){populateRounds(app.profile.rounds);}, 250);
+  setTimeout(function(){addRound(app.round);}, 250);
+  // setTimeout(function(){clearRounds();}, 250);
+  // setTimeout(function(){populateRounds(app.profile.rounds);}, 250);
 };
 
 const createRoundFailure = function (error) {
@@ -69,6 +105,8 @@ module.exports = {
   showAddRoundField,
   hideAddRoundField,
   createRoundSuccess,
+  addRound,
+  addRoundSuccess,
   createRoundFailure,
   // indexRoundsSuccess,
   indexRoundsFailure,
