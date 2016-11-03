86171a873f0253cc5b2e5a64a18416790cb249da
wmcb91 <wmcb91@gmail.com>
2016-11-03 17:10:46 -0400

    Improved breakpoints

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(30);
+	module.exports = __webpack_require__(34);
 
 
 /***/ },
@@ -9935,7 +9935,7 @@
 
 /***/ },
 
-/***/ 30:
+/***/ 34:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
